c9e6d143e43ce33cd15c78f6900c06a8a9d1e88f
Fred Spieler <fspieler@gmail.com>
2019-10-19 22:15:50 -0400

    Create static dir

--- blog/generate_blog.py
+++ blog/generate_blog.py
@@ -8,7 +8,7 @@ from blog.page_converter import PageConverter
 def main():
     shutil.rmtree('public', ignore_errors=True)
     os.makedirs('public')
-    shutil.copytree('css','public/css')
-    shutil.copytree('img','public/img')
-    shutil.copytree('js','public/js')
+    shutil.copytree('static/css','public/css')
+    shutil.copytree('static/img','public/img')
+    shutil.copytree('static/js','public/js')
     PageConverter('content/i-dont-know-php.md', 'public/').convert()
