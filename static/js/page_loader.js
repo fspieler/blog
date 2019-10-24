9eca3b709b71d88ab7a5cd595aee5ea37091cac0
Fred Spieler <fspieler@gmail.com>
2019-10-24 03:13:40 -0400

    SPA wip

--- static/js/page_loader.js
+++ static/js/page_loader.js
@@ -1,3 +1,4 @@
+import updateElements from './color.js'
 function uniqueID () {
     return Date.now().toString()+"-"+Math.random();
 }
@@ -5,13 +6,35 @@ function uniqueID () {
 let last_page_load_request = -1;
 function loadPage (name, newLoad) {
     if(true === newLoad){
-        history.pushState({},'',name);
+        history.pushState({},'',name.replace('content.html',''));
     }
-    uid = uniqueID();
+    let uid = uniqueID();
     last_page_load_request = uid;
     $.get(name, function( data ){
         if(uid == last_page_load_request){
-            $("div.blog-main").append(data);
+            $("div.blog-post").fadeOut(function(){
+                $("div.blog-main").empty();
+                $("div.blog-main").toggle()
+                $("div.blog-main").append(data);
+                interceptLinks();
+                updateElements();
+                $("div.blog-main").fadeIn();
+            });
         }
     });
 }
+
+function interceptLinks(){
+    $('a').click(function(event){
+        let href = $(this).attr('href')+'/content.html';
+        if(this.host == window.location.host && !href.startsWith('#')){
+            event.preventDefault();
+            href = href.replace('//','/');
+            console.log('Intercepting call to '+href);
+            loadPage(href, true);
+        }
+    });
+}
+
+interceptLinks();
+
